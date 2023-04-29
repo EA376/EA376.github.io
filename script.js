a86175ffcc716f4ae3c40e4cc00045157354f49d
EA376 <96833695+EA376@users.noreply.github.com>
2023-04-30 00:04:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ var URLinput = document.querySelector('.URL-input');convertBtn.addEventListener(
     console.log(`URL: ${URLinput.value}`);
     sendURL(URLinput.value);
 });function sendURL(URL) {
-    // We will put code here later
+    fetch(`http://localhost:4000/download?URL=${URL}`, {
+        method:'GET'
+    }).then(res => res.json())
+    .then(json => console.log(json));
 }
